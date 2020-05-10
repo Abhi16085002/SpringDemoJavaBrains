ca3d07b73e50ec7aa201b5e5ca8bc7753274ae10
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-10 22:34:12 +0530

    First commit

---
+++ src/org/koushik/javabrains/DrawingApp.java
@@ -0,0 +1,20 @@
+package org.koushik.javabrains;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+//		Triangle triangle = new Triangle();
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		
+		triangle.draw();
+		
+
+	}
+
+}
