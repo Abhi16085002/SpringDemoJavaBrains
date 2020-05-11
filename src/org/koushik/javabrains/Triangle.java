bdae295bd0f756c0619fa216a55ba5cddd994cea
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-11 11:53:47 +0530

    Spring Tutorial 07 - Injecting Objects

--- src/org/koushik/javabrains/Triangle.java
+++ src/org/koushik/javabrains/Triangle.java
@@ -2,41 +2,47 @@ package org.koushik.javabrains;
 
 public class Triangle {
 	
-	private String type;
-	private int height;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
 	
-	public int getHeight() {
-		return height;
-	}
 	
-	public Triangle (String type) {
-		this.type=type;
-	}
 	
-	public Triangle ( int height) {
-		this.height=height;
-	}
-
-	public Triangle (String type , int height) {
-		this.type=type;
-		this.height=height;
-	}
-	
-	public String getType() {
-		return type;
+	public Point getPointA() {
+		return pointA;
 	}
 
 
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
 
-//	public void setType(String type) {
-//		this.type = type;
-//	}
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
 
 
 
 	public void draw () {
-		System.out.println(getType() + "  Draw Triangle  of height " + getHeight() );
+		System.out.println("PointA = (" + getPointA().getX() + " , " + getPointA().getY() + ")" );
+		System.out.println("PointB = (" + getPointB().getX() + " , " + getPointB().getY() + ")" );
+		System.out.println("PointC = (" + getPointC().getX() + " , " + getPointC().getY() + ")" );
+
 	}
 	
 }
