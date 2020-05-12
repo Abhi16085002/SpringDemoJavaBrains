e548506fda4a15acf4f45c1222dd1e9c9a8ce7d4
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-12 18:37:30 +0530

    Spring Tutorial 20 - Some JSR-250 Annotations

--- src/org/koushik/javabrains/Circle.java
+++ src/org/koushik/javabrains/Circle.java
@@ -1,5 +1,9 @@
 package org.koushik.javabrains;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -16,11 +20,19 @@ public class Circle implements Shape {
 	public Point getCenter() {
 		return center;
 	}
-	@Autowired
-	@Qualifier("circleRelated")
+	@Resource(name="pointC")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
 	
+	@PostConstruct
+	public void initializeCircle() {
+		System.out.println("init method of circle");
+	}
+	
+	@PreDestroy
+	public void destroyCircle() {
+		System.out.println("destroy method of circle");
+	}
 
 }
