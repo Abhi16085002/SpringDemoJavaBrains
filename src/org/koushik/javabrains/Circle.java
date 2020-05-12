bb2136bcc2ebe355781e01c1406225c878471a28
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-12 20:12:46 +0530

    Spring Tutorial 21 - Component and Stereotype Annotations

--- src/org/koushik/javabrains/Circle.java
+++ src/org/koushik/javabrains/Circle.java
@@ -6,7 +6,15 @@ import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+//@Component
+//@Service
+//@Repository
+@Controller
 public class Circle implements Shape {
 	
 	private Point center;
@@ -20,7 +28,7 @@ public class Circle implements Shape {
 	public Point getCenter() {
 		return center;
 	}
-	@Resource(name="pointC")
+	@Resource(name="pointB")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
